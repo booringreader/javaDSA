9e122edb889dd387b354ceb46f668cf707bc6ad0
Shubham Sharma <shubhamsh.z1O1@gmail.com>
2023-10-18 09:09:02 +0530

    duplicate parenthesis

--- Lessons/stack/dupliparenth.java
+++ Lessons/stack/dupliparenth.java
@@ -1,24 +1,31 @@
 import java.util.Stack;
 
-public class dupliparenth {
-    public static boolean dup(String str){
-        Stack <Character> s = new Stack<>();
+class dupliparenth{
+    public static boolean isdup(String str){
+        Stack<Character> s = new Stack<>();
+
         for(int i = 0; i < str.length(); i++){
-            char ch = str.charAt(i);
-            if(ch != ')' || ch != '}' || ch != ']'){
-                s.push(ch);
-            }else{
+            if(str.charAt(i) == ')'){
                 int count = 0;
-                if((s.peek() != '(' && ch==')') || (s.peek() != '{' && ch=='}') || (s.peek() != '[' && ch==']')){
+                while(s.peek() != '('){
                     count++;
                     s.pop();
-                }else if((s.peek()=='(' && ch==')') || (s.peek()=='{' && ch=='}') || (s.peek()=='[' && ch==']')){
-                    if(count < 1){
-                        return true;
-                    }else
-                    return false;
                 }
+                if(count < 1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(str.charAt(i));
             }
         }
+        return false;
+    }
+    public static void main(String args[]){
+        String str = "((a+b))";
+        System.out.println(isdup(str));
+        str = "(a+b)";
+        System.out.println(isdup(str));
     }
 }
