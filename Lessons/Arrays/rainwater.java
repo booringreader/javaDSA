f5d8d22b263214fcd1ed79f9989fb909f04cf59a
Shubham Sharma <shubhamsh.z1O1@gmail.com>
2023-11-21 19:26:53 +0530

    d&c

--- Lessons/Arrays/rainwater.java
+++ Lessons/Arrays/rainwater.java
@@ -26,7 +26,7 @@ public class rainwater{
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[7];
+        int arr[] = new int[9];
 
         for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
