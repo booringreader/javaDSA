b288a355831f76d853f3bcf5571b77511a432557
booringreader <157519312+booringreader@users.noreply.github.com>
2024-06-09 18:46:49 +0530

    01 knapsack - tabulation

--- Lessons/dp/knapsack01.java
+++ Lessons/dp/knapsack01.java
@@ -20,6 +20,33 @@ public class knapsack01 {
             return dp[n][W];
         }
     }
+
+    public static int knapsacktab(int val[], int wt[], int W){
+        int dp[][] = new int[val.length+1][W+1];
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<dp[0].length; j++){
+            dp[0][j] = 0; 
+        }
+
+        for(int i=1; i<val.length+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = val[i-1]; // value of ith item
+                int w = wt[i-1];  // weight of ith item 
+                if(w <= j){ // valid
+                    int incProfit = v + dp[i-1][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{ // invalid condition
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+
+        return dp[val.length][W];
+    }
     public static void main(String args[]){
         int val[] = {15,14,10,45,30};
         int wt[] = {2,5,1,3,4};
@@ -32,6 +59,7 @@ public class knapsack01 {
                 dp[i][j] = -1;
             }
         }
-        System.out.println(knapsack(val, wt, W, val.length, dp));
+        // System.out.println(knapsack(val, wt, W, val.length, dp));
+        System.out.println(knapsacktab(val, wt, W));
     }
 }
