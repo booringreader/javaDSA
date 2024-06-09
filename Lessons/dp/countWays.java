31eacc0ecf98edd08dc5d496931df7d811f48c93
booringreader <157519312+booringreader@users.noreply.github.com>
2024-06-09 11:54:30 +0530

    recursion + memoization

--- Lessons/dp/countWays.java
+++ Lessons/dp/countWays.java
@@ -1,17 +1,24 @@
 package dp;
 
+import java.util.Arrays;
 public class countWays {
-    public static int countways(int n){
+    public static int countways(int ways[], int n){
         if(n==0){
             return 1;
         }else if(n<0){
             return 0;
         }
 
-        return countways(n-1) + countways(n-2);
+        if(ways[n] != -1){
+            return ways[n];
+        }
+        ways[n] = countways(ways, n-1) + countways(ways, n-2);
+        return ways[n];
     }
     public static void main(String[] args){
         int n=5;
-        System.out.println(countways(n));
+        int ways[] = new int[n+1];
+        Arrays.fill(ways, -1);
+        System.out.println(countways(ways, n));
     }
 }
